33dc6bbd352d6e15f79fe56d3d3bafd03baeece4
Westly Wright <westly.wright@gmail.com>
2016-04-13 16:35:38 -0700

    update dependecies, rework componet file, remove babel

--- components/component.js
+++ components/component.js
@@ -1,6 +1,15 @@
-import Ember from 'ember';
-import Driver from 'ui/mixins/driver';
+ /*Do not change anything below (dont worry we'll swap the component name via the cli)*/
+define('ui/components/drivers/<driver-name>/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
 
-export default Ember.Component.extend(Driver, {
-  world: 'world'
+  exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
+ /*Do not change anything above*/
+
+     //Normal ember component hooks and methods apply below
+     //Write your componet here
+    driverName: '<your-driver-name>',
+    bootstrap: (function () {
+       /*do stuff on init if you want*/
+    }).on('init')
+    // Normal ember component hooks and methods apply above
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,24 @@
 "use strict";
 /* jshint node: true */
 const gulp        = require('gulp');
-const clean = require('gulp-clean');
-const babel       = require('gulp-babel');
+const clean       = require('gulp-clean');
 const gulpConcat  = require('gulp-concat');
 const gulpConnect = require('gulp-connect');
-const gulpHint    = require('gulp-jshint');
-const gulpMap     = require('gulp-sourcemaps');
 const emCompiler  = require('./bower_components/ember/ember-template-compiler');
 const htmlbars    = require('gulp-htmlbars-compiler');
-//const defineModule = require('gulp-define-module');
-var wrapAmd = require('gulp-wrap-amd');
-var replace = require('gulp-replace');
-//var handlebars = require('gulp-handlebars');
-//var Htmlbars = require('ember-cli-htmlbars');
-//var compiler = new Htmlbars();
+const wrapAmd     = require('gulp-wrap-amd');
+const replace     = require('gulp-replace');
+const argv        = require('yargs').argv;
 
 
-const pkg = require('./package.json');
+const DIST        = 'dist/';
+const TMP         = 'tmp/';
+let DRIVER_NAME   = argv.name;
 
-const DIST = 'dist/';
-const TMP  = 'tmp/';
-let VERSION_DIST = DIST+pkg.version+'/';
-
-
-gulp.task('clean', function() {
-  return gulp.src([DIST, TMP], {read: false})
-  .pipe(clean());
-});
-
-
+if (!DRIVER_NAME) {
+  console.log('Please include a driver name with the --name flag');
+  return false;
+}
 gulp.task('server', ['build'], function() {
   return gulpConnect.server({
     root: [DIST],
@@ -37,34 +26,27 @@ gulp.task('server', ['build'], function() {
   });
 });
 
+gulp.task('clean', function() {
+  return gulp.src([DIST, TMP], {read: false})
+  .pipe(clean());
+});
+
 gulp.task('js', ['clean'], function() {
   return gulp.src([
-    'component/*.js'
+    'components/*.js'
   ])
-  .pipe(babel({
-    presets: ['es2015-without-strict']
-  }))
+  .pipe(replace(
+    '<driver-name>', DRIVER_NAME
+  ))
   .pipe(gulpConcat('component.js',{newLine: ';\n'}))
   .pipe(gulp.dest(TMP));
 });
 
 gulp.task('templates', ['js'], function() {
   // Load templates from the source/templates/ folder relative to where gulp was executed
-  return gulp.src('component/**/*.hbs')
+  return gulp.src('components/**/*.hbs')
   // Compile each Handlebars template source file to a template function using Ember's Handlebars
-  //.pipe(handlebars({
-  //handlebars: require('ember-handlebars'),
-  //compiler: compiler.processString
-  //}))
   .pipe(htmlbars({compiler: emCompiler}))
-  // Concatenate down to a single file
-  .pipe(gulpConcat('template.js'))
-  // Write the output into the templates folder
-  .pipe(gulp.dest(TMP));
-});
-
-gulp.task('build', ['templates'], function() {
-  return gulp.src([`${TMP}/*.js`])
   .pipe(wrapAmd({
     deps: ['exports', 'ember', 'ui/mixins/driver'],          // dependency array
     params: ['exports', '_ember', '_uiMixinsDriver'],        // params for callback
@@ -72,17 +54,20 @@ gulp.task('build', ['templates'], function() {
     modulePrefix: 'ui/components/drivers/'
   }))
   .pipe(replace(
-    "return Ember.TEMPLATES['driver-template']", 'exports["default"]'
+    "return Ember.TEMPLATES['template']", 'exports["default"]'
   ))
+  .pipe(replace(
+    '../components', DRIVER_NAME
+  ))
+  // Concatenate down to a single file
+  .pipe(gulpConcat('template.js'), {newLine: ';\n'})
+  // Write the output into the templates folder
+  .pipe(gulp.dest(TMP));
+});
+
+gulp.task('build', ['templates'], function() {
+  return gulp.src([`${TMP}/*.js`])
   .pipe(gulpConcat('component.js',{newLine: ';\n'}))
   .pipe(gulp.dest(DIST));
 
 });
-
-//gulp.task('templates', function() {
-//return gulp.src(['component/*.hbs'])
-//.pipe(htmlbars({compiler: emCompiler}))
-//.pipe(defineModule('amd'))
-//.pipe(gulpConcat('driver-template.js',{newLine: ';\n'}))
-//.pipe(gulp.dest(TMP));
-//});
