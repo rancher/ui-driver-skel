096e17fd09578088f4e7071e4e96cdf45831c0b9
Vincent Fiduccia <vincent@rancher.com>
2016-04-18 17:47:17 -0700

    Cleanup & Rename (#1)
    - s/components/component
    - Load the driver name from package.json
    - Use local npm-installed gulp instead of global
    - Generate CSS and assets
    - Update readme with more detail on usage

---
+++ component/component.js
@@ -0,0 +1,52 @@
+/* v----- Do not change anything between here
+ *       (the DRIVERNAME placeholder will be automatically replaced during build) */
+define('ui/components/drivers/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
+
+  exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
+    driverName: 'driver-%%DRIVERNAME%%',
+/* ^--- And here */
+
+    // Write your component here, starting with setting 'model' to a machine with your config populated
+    bootstrap: function() {
+      let config = this.get('store').createRecord({
+        type        : '%%DRIVERNAME%%Config',
+        size        : 512,
+      });
+
+      this.set('model', this.get('store').createRecord({
+        type: 'machine',
+        '%%DRIVERNAME%%Config': config,
+      }));
+    }.on('init'),
+
+    // Add custom validation beyond what can be done from the config API schema
+    validate() {
+      // Get generic API validation errors
+      this._super();
+      var errors = this.get('errors')||[];
+
+      // Add more specific errors
+
+      // Check something and add an error entry if it fails:
+      if ( parseInt(this.get('model.%%DRIVERNAME%%Config.size'),10) < 1024 )
+      {
+        errors.push('Size must be at least 1024 MB');
+      }
+
+      // Set the array of errors for display,
+      // and return true if saving should continue.
+      if ( errors.get('length') )
+      {
+        this.set('errors', errors);
+        return false;
+      }
+      else
+      {
+        this.set('errors', null);
+        return true;
+      }
+    },
+
+    // Any computed properties or custom logic can go here
+  });
+});
