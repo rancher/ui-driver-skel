97ba39b5e08ec91fd17dae8d6f3d603d114c259a
Max Schmitt <max@schmitt.mx>
2018-03-27 13:19:37 +0200

    Updated for rancher 2

--- component/component.js
+++ component/component.js
@@ -1,49 +1,67 @@
 /* v----- Do not change anything between here
  *       (the DRIVERNAME placeholder will be automatically replaced during build) */
-define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'ui/mixins/driver', 'ui/utils/constants'], function (exports, _ember, _uiMixinsDriver, _uiUtilsConstants) {
+define('ui/components/node-driver/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'shared/components/node-driver/driver-%%DRIVERNAME%%/component'], function (exports, _ember, _component) {
+  exports['default'] = _component['default'];
+});
 
+define('shared/components/node-driver/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'shared/mixins/node-driver', 'shared/components/node-driver/driver-%%DRIVERNAME%%/template', 'ui/utils/constants'], function (exports, _ember, _uiMixinsDriver, _template, _uiUtilsConstants) {
+  /* ^--- And here */
+
+  // You can put embmer object here
+  var computed = Ember.computed;
+  var get = Ember.get;
+  var set = Ember.set;
+  var alias = Ember.computed.alias;
+
+  /* v----- Do not change anything between here
+   *       (the DRIVERNAME placeholder will be automatically replaced during build) */
   exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
+    layout: _template.default,
     driverName: '%%DRIVERNAME%%',
     needAPIToken: true,
+    config: alias('model.%%DRIVERNAME%%Config'),
+    /* ^--- And here */
 
+    // Write your component here, starting with setting 'model' to a machine with your config populated
     bootstrap: function () {
-      let config = this.get('store').createRecord({
+      let config = get(this, 'globalStore').createRecord({
         type: '%%DRIVERNAME%%Config',
-        apiToken: '',
         serverType: 'cx21', // 4 GB Ram
-        serverLocation: 'fsn1', // Nuremberg
-        image: 'ubuntu-16.04'
+        serverLocation: 'nbg1', // Nuremberg
+        image: 'ubuntu-16.04',
+        userData: ''
       });
 
-      let type = 'host';
-      if (!this.get('useHost')) {
-        type = 'machine';
-      }
-
-      this.set('model', this.get('store').createRecord({
-        type: type,
-        // Without the overlay storage driver, only Ubuntu works. Debian, CentOS, Fedora won't.
-        engineStorageDriver: 'overlay',
-        '%%DRIVERNAME%%Config': config,
-      }));
+      set(this, 'model.%%DRIVERNAME%%Config', config);
     },
 
+    // Add custom validation beyond what can be done from the config API schema
     validate() {
+      // Get generic API validation errors
       this._super();
-      let errors = this.get('errors') || [];
-
-      let apiToken = this.get('model.hetznerConfig.apiToken') || '';
-      if (apiToken && apiToken.length !== 64) {
-        errors.push('That does not look like a valid access token');
+      var errors = get(this, 'errors') || [];
+      if (!get(this, 'model.name')) {
+        errors.push('Name is required');
       }
 
-      if (errors.get('length')) {
-        this.set('errors', errors);
+      // Add more specific errors
+
+      // Check something and add an error entry if it fails:
+      if (parseInt(get(this, 'config.memorySize'), 10) < 1024) {
+        errors.push('Memory Size must be at least 1024 MB');
+      }
+
+      // Set the array of errors for display,
+      // and return true if saving should continue.
+      if (get(errors, 'length')) {
+        set(this, 'errors', errors);
         return false;
       }
-      return true;
+      else {
+        set(this, 'errors', null);
+        return true;
+      }
     },
-
     actions: {
       getData() {
         this.set('gettingData', true);
@@ -69,12 +87,12 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
       onImageChange(ev) {
         this.set('model.hetznerConfig.image', ev.target.value);
         // Use the most recent version if it's Fedora. The default (recommend, 1.12) and all other won't work with Fedora.
-        if (/fedora/i.test(ev.target.value)) {
-          this.set('model.engineInstallUrl', 'https://get.docker.com')
-        } else {
-          let defaultEngineInstallURL = this.get('settings.' + _uiUtilsConstants['default'].SETTING.ENGINE_URL);
-          this.set('model.engineInstallUrl', defaultEngineInstallURL)
-        }
+        // if (/fedora/i.test(ev.target.value)) {
+        //   this.set('model.engineInstallUrl', 'https://get.docker.com')
+        // } else {
+        //   let defaultEngineInstallURL = this.get('settings.' + _uiUtilsConstants['default'].SETTING.ENGINE_URL);
+        //   this.set('model.engineInstallUrl', defaultEngineInstallURL)
+        // }
       }
     },
 
@@ -85,5 +103,6 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
         },
       }).then(res => res.ok ? res.json() : Promise.reject(res.json()));
     }
+    // Any computed properties or custom logic can go here
   });
 });
