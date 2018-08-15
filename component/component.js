21cee3b4d888a6bc87f211db1ccb5c1c7018fa84
Westly Wright <westly.wright@gmail.com>
2018-08-15 16:28:05 -0700

    More component changes

--- component/component.js
+++ component/component.js
@@ -1,18 +1,22 @@
 /*!!!!!!!!!!!Do not change anything between here (the DRIVERNAME placeholder will be automatically replaced at buildtime)!!!!!!!!!!!*/
 import NodeDriver from 'shared/mixins/node-driver';
 
+// do not remove LAYOUT, it is replaced at build time with a base64 representation of the template of the hbs template
+// we do this to avoid converting template to a js file that returns a string and the cors issues that would come along with that
 const LAYOUT;
+/*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+
+/*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
+// EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import them via modules, since the dependencies exist in rancher we dont want to expor the modules in the amd def
 const computed     = Ember.computed;
 const get          = Ember.get;
 const set          = Ember.set;
 const alias        = Ember.computed.alias;
 const service      = Ember.inject.service;
+
 const defaultRadix = 10;
 const defaultBase  = 1024;
-/*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
-
-
-/*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
 /*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
 
 
@@ -21,11 +25,9 @@ const defaultBase  = 1024;
 export default Ember.Component.extend(NodeDriver, {
   driverName: '%%DRIVERNAME%%',
   config:     alias('model.%%DRIVERNAME%%Config'),
-  /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
   app:        service(),
 
   init() {
-    /*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
     // This does on the fly template compiling, if you mess with this :cry:
     const decodedLayout = window.atob(LAYOUT);
     const template      = Ember.HTMLBars.compile(decodedLayout, {
@@ -34,25 +36,27 @@ export default Ember.Component.extend(NodeDriver, {
     set(this,'layout', template);
 
     this._super(...arguments);
-    /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
 
-    const config = get(this, 'globalStore').createRecord({
-      type:       '%%DRIVERNAME%%Config',
-      cpuCount:   2,
+  },
+  /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+  // Write your component here, starting with setting 'model' to a machine with your config populated
+  bootstrap: function() {
+    // bootstrap is called by rancher ui on 'init', you're better off doing your setup here rather then the init function to ensure everything is setup correctly
+    let config = get(this, 'globalStore').createRecord({
+      type: '%%DRIVERNAME%%Config',
+      cpuCount: 2,
       memorySize: 2048,
     });
 
     set(this, 'model.%%DRIVERNAME%%Config', config);
-
   },
 
   // Add custom validation beyond what can be done from the config API schema
   validate() {
     // Get generic API validation errors
     this._super();
-
     var errors = get(this, 'errors')||[];
-
     if ( !get(this, 'model.name') ) {
       errors.push('Name is required');
     }
@@ -60,7 +64,7 @@ export default Ember.Component.extend(NodeDriver, {
     // Add more specific errors
 
     // Check something and add an error entry if it fails:
-    if ( parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase ) {
+    if ( parseInt(get(this, 'config.memorySize'),10) < 1024 ) {
       errors.push('Memory Size must be at least 1024 MB');
     }
 
@@ -73,6 +77,7 @@ export default Ember.Component.extend(NodeDriver, {
       set(this, 'errors', null);
       return true;
     }
-
   },
+
+  // Any computed properties or custom logic can go here
 });
